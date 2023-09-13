3c42af9225eef0c54be511e1eb9f0421b555d467
Sudhanshu Poonia <sudhnsh16@gmail.com>
2023-09-13 16:27:29 +0530

    added ratio_cal

--- inputdata.py
+++ inputdata.py
@@ -1,4 +1,4 @@
-text1="This is the first Text"
-text2="This is the second Text"
+text1="This is the firstyy Text"
+text2="This is the secondyy Text"
 path="input.jpg"
 font_used="font\Montserrat-Light.ttf"
--- main.py
+++ main.py
@@ -7,40 +7,47 @@ def findfont(text_bbox,box_width,box_height,font,fontsize,text,draw):
         text_bbox = draw.textbbox((0, 0), text, font=font)
     return font
 
+def ratio_cal(width,height):
+    ratio= height/width
+    if height>width:
+        new_height=730
+        new_width=round(730/ratio)
+    else :
+        new_width=980
+        new_height=round(980*ratio)
+    return (new_width,new_height)
+
 def imagetranform(text1,text2,path):
     # Image Open
     image = Image.open(path)
     print(image.size)
-    right = 0
-    left = 100
-    top = 50
-    bottom = 300
+    new_width,new_height=ratio_cal(image.size[0],image.size[1])
+    image = image.resize((new_width,new_height))
+    print(image.size)
     width, height = image.size
     # Adding the Padding for the text
-    new_width = width + right + left
-    new_height = height + top + bottom
-    result = Image.new(image.mode, (new_width, new_height), (255, 255, 255))
+    result = Image.new(image.mode, (1080,1080), (255, 255, 255))
     draw = ImageDraw.Draw(result)
     # Adding the grey box behind
-    draw.rectangle((0, 0, new_width, height/2+50), fill=ImageColor.getrgb("#f4f4f6"))
+    draw.rectangle((0, 0, 1080, height/2+50), fill=ImageColor.getrgb("#f4f4f6"))
     # Adding the Image
-    result.paste(image, (left, top))
+    result.paste(image, (1080-width, 50))
     # First font calculation
     font = ImageFont.truetype(font_used, 45)
     draw = ImageDraw.Draw(result)
-    draw.rectangle((0, height+50, new_width/2, height+100), fill=ImageColor.getrgb("#dfcad1"))
-    draw.rectangle((new_width/2, height+50, new_width, height+100), fill=ImageColor.getrgb("#2b1e16"))
+    draw.rectangle((0, height+50, 540, height+100), fill=ImageColor.getrgb("#dfcad1"))
+    draw.rectangle((540, height+50, 1080, height+100), fill=ImageColor.getrgb("#2b1e16"))
     text_bbox = draw.textbbox((10, 0), text1, font=font)
-    font=findfont(text_bbox,width-500,45,font,45,text1,draw)
+    font=findfont(text_bbox,580,45,font,45,text1,draw)
     # Adding Text1
-    draw.text((new_width/4,new_height-270), text1, font=font, fill= ImageColor.getrgb("#fffbfe"),anchor='mm') 
-    box_width,box_height=new_width,250
+    draw.text((270,height+72.5), text1, font=font, fill= ImageColor.getrgb("#fffbfe"),anchor='mm') 
+    box_width,box_height=1080,250
     # Second font calculation
     font = ImageFont.truetype(font_used, 150)
     text_bbox = draw.textbbox((0, 0), text2, font=font)
     font = findfont(text_bbox,box_width,box_height,font,150,text2,draw)
     # Adding Text2
-    draw.text((new_width/2,height+250), text2, font=font, fill= ImageColor.getrgb("#48433f"),anchor='mm') 
+    draw.text((540,height+250), text2, font=font, fill= ImageColor.getrgb("#48433f"),anchor='mm') 
     result.save('output.png')
 
 
